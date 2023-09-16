f9d904305f1dc6788336abc6a4421ff4af4a43e1
AxelMatthew12 <143260705+AxelMatthew12@users.noreply.github.com>
2023-09-16 17:13:45 +0700

    int

--- Main.java
+++ Main.java
@@ -1,5 +1,21 @@
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        String burger = "Burger";
+        String Pizza = "Pizza";
+        String Taco = "Taco";
+
+
+        int hargaBurger = 10_000;
+        int hargaPizza = 15_000;
+        int hargaTaco = 15_000;
+
+
+        System.out.println("======MENU=======");
+        System.out.println("1." + burger +"=" + hargaBurger);
+        System.out.println("2." + Pizza  +"=" + hargaPizza);
+        System.out.println("3." + Taco   +"=" + hargaTaco);
+        System.out.println("0.Keluar");
+
     }
 }
