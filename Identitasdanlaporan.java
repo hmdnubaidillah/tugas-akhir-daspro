cc7e8ca2625c7af0c27b8ff4808371d5af9fe8a7
AxelMatthew12 <143260705+AxelMatthew12@users.noreply.github.com>
2023-12-05 22:26:46 +0700

    Tambahan untuk hari ini

--- Identitasdanlaporan.java
+++ Identitasdanlaporan.java
@@ -7,9 +7,9 @@ public class Identitasdanlaporan {
         int kesempatan=0;
 
         while (kesempatan < 20) {
-        System.out.println("SELAMAT DATANG DI LAPORAN RESTO");
-        System.out.println("===============================");
-        System.out.println("====       WELCOME         ====");
+        System.out.println("=========SELAMAT DATANG========");
+        System.out.println("=========            ==========");
+        System.out.println("====        WELCOME        ====");
         System.out.println("===============================\n");
         System.out.println("SILAHKAN LOGIN TERLEBIH DAHULU");
         System.out.println("MASUKKAN ID: ");
@@ -36,13 +36,18 @@ public class Identitasdanlaporan {
     System.out.println("====SELAMAT DATANG====");
     System.out.println("1.TENTANG MENU");
     System.out.println("2.CEK MENU");
-    System.out.println("3.TAMBAHKAN MENU");
-    System.out.println("4.KURANGI MENU");
+    System.out.println("3.LAPORAN PENDAPATAN");
     int pilihan= sc.nextInt();
+    if (pilihan==1) {
+        tentangMenu();
+        
+    }
+
+    }
     
 
-   }
-
+   
+// DALAMANNYA  MENU 1
 static void tentangMenu() {
     Scanner sc = new Scanner(System.in);
     System.out.println("=====DAFTAR MENU=====");
@@ -51,19 +56,91 @@ static void tentangMenu() {
     System.out.println("ES KRIM");
     String pilihan2 = sc.nextLine();
 
-    // Menggunakan pernyataan if-else untuk menampilkan informasi sesuai dengan pilihan
+    
     if (pilihan2.equalsIgnoreCase("Burger")) {
-        System.out.println("Informasi mengenai Burger:" +
-        "NAMA MENU : BURGER "+ 
-        "JENIS MENU: MAKANAN BERAT"); // Tambahkan informasi mengenai burger di sini
+        System.out.println("Informasi mengenai Burger:\n" +
+        "NAMA MENU : BURGER\n"+ 
+        "JENIS MENU: MAKANAN BERAT\n"+
+        "STATION: DAPUR 2B\n"); 
     } else if (pilihan2.equalsIgnoreCase("TACO")) {
-        System.out.println("Informasi mengenai TACO: ..."); // Tambahkan informasi mengenai TACO di sini
+        System.out.println("Informasi mengenai TACO: ..."); 
     } else if (pilihan2.equalsIgnoreCase("ES KRIM")) {
-        System.out.println("Informasi mengenai ES KRIM: ..."); // Tambahkan informasi mengenai ES KRIM di sini
+        System.out.println("Informasi mengenai ES KRIM: ..."); 
     } else {
         System.out.println("Pilihan tidak valid");
     }
 }
+static void cekmenu(){
+    Scanner sc= new Scanner(System.in);
+    System.out.println("====BERIKUT TAMPILAN MENU TERSEDIA SAAT INI====");
+    System.out.println("====                BURGER                 ====");
+    System.out.println("====                 TACO                  ====");
+    System.out.println("====                ESKRIM                 ====");
+    System.out.println("====                 ====                  ====");
+    System.out.println("===============================================");
+    String pilihan3=sc.nextLine();
+   switch (pilihan3.toLowerCase()) {
+    case "Burger":
+    tampilkanResepBurger();
+    break;
+    case "TACO":
+    tampilkanResepTaco();
+    break;
+    case "eskrim":
+    tampilkanResepEskrim();
+    break;
+    default:
+    System.out.println("Menu tidak valid.Silahkanpilih kembali ");
+    cekmenu();
+    break;
+   }
+    }
+
+    public static void tampilkanResepBurger(){
+        System.out.println("Resep Burger:");
+        System.out.println("1. Siapkan roti burger, patty daging, keju, sayuran, dan saus.");
+        System.out.println("2. Panggang patty daging hingga matang.");
+        System.out.println("3. Susun roti, patty, keju, dan sayuran.");
+        System.out.println("4. Tambahkan saus sesuai selera.");
+    }
+
+    public static void tampilkanResepTaco() {
+        System.out.println("Resep Taco:");
+        System.out.println("1. Siapkan kulit tortilla, daging cincang, sayuran, dan saus.");
+        System.out.println("2. Tumis daging cincang hingga matang.");
+        System.out.println("3. Letakkan daging di atas kulit tortilla dan tambahkan sayuran.");
+        System.out.println("4. Gulung tortilla dan sajikan.");
+    }
+
+    public static void tampilkanResepEskrim() {
+        System.out.println("Resep Eskrim:");
+        System.out.println("1. Siapkan susu, gula, telur, dan perisa eskrim.");
+        System.out.println("2. Campurkan susu, gula, dan telur dalam panci.");
+        System.out.println("3. Panaskan aduk-aduk hingga meleleh.");
+        System.out.println("4. Tambahkan perisa eskrim dan dinginkan dalam freezer.");
+    }
+    
+static int  LaporanPendapatan(int pendapatan,int kerugian,int imbang){
+    Scanner sc= new Scanner(System.in);
+    System.out.println("BERIKUT ADALAH UNTUK PENDAPATAN PER HARI INI: ");
+    System.out.println("Masukkan total pendapatan hari ini: ");
+    int barangterjual= sc.nextInt();
+    System.out.println("Masukkan modal jualan untuk hari ini: ");
+    int modaljualan= sc.nextInt();
+    pendapatan= barangterjual-modaljualan;
+    return pendapatan;
+    
+
+  
 }
+public static void main(String[] args) {
+    Login();
+    Inputmenu();
+    tentangMenu();
+    LaporanPendapatan(0, 0, 0);
+    System.out.println("Total pendapatan"+LaporanPendapatan(0, 0, 0));
+}
+}
+
 
 
